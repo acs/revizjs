975030e5a4c161eda797e92c88e11df0b0203ba7
Alvaro del Castillo <acs@bitergia.com>
2014-11-15 00:56:07 +0100

    Added first version of a conceptual dash using angularjs.

---
+++ browserng/app/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+
+var myApp = angular.module('myApp', [
+  'ngRoute'
+]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/scm', {
+          templateUrl: 'scm/overview.html',
+          // controller: 'PhoneListCtrl'
+      }).
+      when('/scm/companies', {
+          templateUrl: 'scm/companies/scm_companies.html',
+          // controller: 'PhoneListCtrl'
+      }).
+      when('/scm/repos', {
+          templateUrl: 'scm/repos/scm_repos.html',
+          // controller: 'PhoneListCtrl'
+    }).
+    otherwise({redirectTo: '/scm'});
+}]);
